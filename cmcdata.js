245c67d0f8b41917adf99da2935d50740b160081
Ahmer Malik <ahmer9800@gmail.com>
2018-06-09 18:09:25 -0500

    updates

--- cmcdata.js
+++ cmcdata.js
@@ -1,11 +1,9 @@
 var request = require('request');
+var db = require('./models');
 
-
-request('https://api.coinmarketcap.com/v1/ticker/?limit=10', function (error, response, body) {
+request('https://api.coinmarketcap.com/v2/ticker/?limit=10', function (error, response, body) {
     console.log('error:', error); // Print the error if one occurred
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
     console.log('data:', body); // Print the Json data response by coinmarket cap .
 
 });
-
-
